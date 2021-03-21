4375516130cc74a503583fac9f79097cbbf1f030
Rinkachirukuki <55659018+Rinkachirukuki@users.noreply.github.com>
2021-03-21 16:21:25 +0500

    Initialization
    Java
    Spring MVC

---
+++ src/main/java/com/SoftwareDevelopment/TrComp/TrCompApplication.java
@@ -0,0 +1,13 @@
+package com.SoftwareDevelopment.TrComp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrCompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoftwareDevelopment/TrComp/TrCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareDevelopment.TrComp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
